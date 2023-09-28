2b386091eee4a7484fce8590d15b34d411f85f9e
Matheus Migge <matheusmigge@gmail.com>
2023-09-28 12:41:14 -0300

    Initial Commit

---
+++ PraQuemEuRezo/PraQuemEuRezoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PraQuemEuRezoApp.swift
+//  PraQuemEuRezo
+//
+//  Created by Migge on 28/09/23.
+//
+
+import SwiftUI
+
+@main
+struct PraQuemEuRezoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
